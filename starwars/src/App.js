bacea34e083fea2faef13d090695c5cb9d5f9ac1
juliusheese <julius.42.shapiro@gmail.com>
2020-08-30 18:32:35 -0500

    axios and useeffect set up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [Data, setData] = useState(["Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...",])
+  const [data, setData] = useState(["Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...", "Loading...",])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const getData = () => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => setData(res))
+      .catch(err => console.log(err));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
